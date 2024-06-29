6297387abd9ab017d6f94c6a7ed9873bb4f3a931
Farhan Ali Mirza <farhanalimirza2003@gmail.com>
2024-06-30 03:15:04 +0530

    changes

---
+++ app/view/page.js
@@ -0,0 +1,15 @@
+// "use client";
+// import { Worker } from "@react-pdf-viewer/core";
+// import { Viewer } from '@react-pdf-viewer/core';
+// import '@react-pdf-viewer/core/lib/styles/index.css';
+// const page = () => {
+//   return (
+//     <div className="bg-slate-600">
+//       <Worker workerUrl="https://unpkg.com/pdfjs-dist@3.4.120/build/pdf.worker.min.js">
+//       <Viewer theme='dark' fileUrl="/FarhanResume.pdf" />;
+//       </Worker>
+//     </div>
+//   );
+// };
+
+// export default page;
