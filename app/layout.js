bd3419a5270df8c2bccc6870a3cee9981f1fc02f
Aman Singh - Xcriminal <75941853+xcriminal1@users.noreply.github.com>
2024-07-18 20:27:04 +0000

    updated title

--- app/layout.js
+++ app/layout.js
@@ -4,7 +4,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Farhan Ali Mirza",
+  title: "Aman Singh",
   description: "Resume/CV",
 };
 
